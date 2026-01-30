8ef9e9a28d4597a0ded38224852f8a87476beddf
rahulkumar2027-spec <rahulkumarece2027@gmail.com>
2026-01-30 16:35:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ st.set_page_config(page_title="Iris Classifier", page_icon="🌸")
 @st.cache_resource
 def load_model():
     # Update the path to look for the .pkl file
-    model_path = "iris_model.pkl" 
+    model_path = "model.pkl" 
     if os.path.exists(model_path):
         with open(model_path, 'rb') as file:
             return pickle.load(file)
